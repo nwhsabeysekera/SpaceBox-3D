53e63fb9ca5a6878f98ee376863681531b97d7d6
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-03-29 12:51:28 +0530

    Create New UI Create events for UI Create PreviewButton Create Panel Create Methods in Dropdwon , PageLoad

--- SpaceBox-3D/Shapes.aspx.cs
+++ SpaceBox-3D/Shapes.aspx.cs
@@ -11,7 +11,10 @@ namespace SpaceBox_3D
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            Length.Visible = true;
+            Width.Visible = true;
+            Radius.Visible = false;
+            Base.Visible = false;
         }
 
         protected void Length_TextChanged(object sender, EventArgs e)
@@ -19,15 +22,107 @@ namespace SpaceBox_3D
 
         }
 
-        protected void btnApply_Click(object sender, EventArgs e)
-        {
-           
-        }
-
         protected void Width_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        protected void Radius_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Base_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnApply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShowPreview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPrint_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Circle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Length.Visible = false;
+            Width.Visible = false;
+            Radius.Visible = true;
+            Base.Visible = false;
+
+
+        }
+        protected void Rectangle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Length.Visible = true;
+            Width.Visible = true;
+            Radius.Visible = false;
+            Base.Visible = false;
+
+
+        }
+        protected void Triangle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Length.Visible = true;
+            Width.Visible = false;
+            Radius.Visible = false;
+            Base.Visible = true;
+
+
+        }
+
+        protected void SelectShape_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected shape from the dropdown list
+            string selectedShape = SelectShape.SelectedValue;
+
+            // Load the parameters for the selected shape
+            if (selectedShape == "Rectangle")
+            {
+                // Show input fields for length and width
+               
+                Length.Visible = true;
+                Width.Visible = true;
+                Radius.Visible = false;
+                Base.Visible = false;
+            }
+            else if (selectedShape == "Circle")
+            {
+                // Show input field for radius
+                Length.Visible = false;
+                Width.Visible = false;
+                Radius.Visible = true;
+                Base.Visible = false;
+            }
+            else if (selectedShape == "Triangle")
+            {
+                // Show input fields for base and height
+                Length.Visible = true;
+                Width.Visible = false;
+                Radius.Visible = false;
+                Base.Visible = true;
+            }
+        }
     }
+    
     
 }
